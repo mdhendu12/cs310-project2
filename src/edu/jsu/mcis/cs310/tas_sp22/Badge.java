3a760886d7e4c7671508681ed59ecbeccb4d124c
Hunter Wert <hwert@stu.jsu.edu>
2022-02-28 12:37:38 -0600

    Created Badge Class

---
+++ src/edu/jsu/mcis/cs310/tas_sp22/Badge.java
@@ -0,0 +1,25 @@
+
+package edu.jsu.mcis.cs310.tas_sp22;
+import java.util.HashMap; 
+
+
+public class Badge 
+{
+    private String id, description; 
+    
+ public Badge(HashMap<String, String> params)
+ {
+     this.id = params.get("id");
+     this.description= params.get("description"); 
+ }
+ 
+ public String getId()
+ {
+     return id;
+ }
+ 
+ public String getDescription() 
+ {
+     return description;          
+ }
+}
