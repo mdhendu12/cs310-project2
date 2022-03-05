8e34da4759498814f20f80ad8eb9913428711e01
Hunter Wert <hwert@stu.jsu.edu>
2022-03-05 12:49:30 -0600

    changed variable employeeid to employeetypeid

--- src/edu/jsu/mcis/cs310/tas_sp22/Employee.java
+++ src/edu/jsu/mcis/cs310/tas_sp22/Employee.java
@@ -6,13 +6,13 @@ import java.util.HashMap;
 public class Employee 
 {
     private String badge, first, middle, last; 
-    private int id, employeeid, department, shift; 
+    private int id, employeetypeid, department, shift; 
     private LocalTime active, inactive; 
     
  public Employee(HashMap<String, String> s, HashMap<String, Integer> i, HashMap<String, LocalTime> l)
  {
      this.id = i.get("id");
-     this.employeeid = i.get("employeeid");
+     this.employeetypeid = i.get("employeetypeid");
      this.department = i.get("department"); 
      this.shift = i.get("shift"); 
      this.badge = s.get("badge"); 
@@ -30,7 +30,7 @@ public class Employee
  
  public int getEmployee()
  {
-     return employeeid;
+     return employeetypeid;
  }
  
  public int getDepartment()
@@ -79,7 +79,7 @@ public class Employee
  {
      StringBuilder result =  new StringBuilder(); 
      result.append("#").append(id).append(" (").append(last).append(" ").append(first);
-     result.append(" ").append(middle).append(") : ").append(employeeid).append(", : ");
+     result.append(" ").append(middle).append(") : ").append(employeetypeid).append(", : ");
      result.append(department).append(", : ").append(shift).append(", : ").append(active); 
      result.append(inactive).append(","); 
      
