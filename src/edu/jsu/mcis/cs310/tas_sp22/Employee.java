919d6ac25786d1446f6b2fc1e4a20fd4e8ca8f31
Hunter Wert <hwert@stu.jsu.edu>
2022-03-05 12:06:29 -0600

    Same as last commit

--- src/edu/jsu/mcis/cs310/tas_sp22/Employee.java
+++ src/edu/jsu/mcis/cs310/tas_sp22/Employee.java
@@ -1,34 +1,88 @@
 
 package edu.jsu.mcis.cs310.tas_sp22;
+import java.time.*;
 import java.util.HashMap;
 
 public class Employee 
 {
-    private String description; 
-    private int id; 
+    private String badge, first, middle, last; 
+    private int id, employee, department, shift; 
+    private LocalTime active, inactive; 
     
- public Employee(HashMap<String, String> params, HashMap<String, Integer> larams)
+ public Employee(HashMap<String, String> s, HashMap<String, Integer> i, HashMap<String, LocalTime> l)
  {
-     this.id = larams.get("id");
-     this.description= params.get("description"); 
+     this.id = i.get("id");
+     this.employee = i.get("employee");
+     this.department = i.get("department"); 
+     this.shift = i.get("shift"); 
+     this.badge = s.get("badge"); 
+     this.first = s.get("fist"); 
+     this.middle = s.get("middle"); 
+     this.last = s.get("last"); 
+     this.active = l.get("active"); 
+     this.inactive = l.get("inactive"); 
  }
  
- public Integer getId()
+ public int getId()
  {
      return id;
  }
  
- public String getDescription() 
+ public int getEmployee()
  {
-     return description;          
+     return employee;
+ }
  
-}
+ public int getDepartment()
+ {
+     return department; 
+ }
+ 
+ public int getShift()
+ {
+     return shift;
+ }
+ 
+ public String getBadge()
+ {
+     return badge; 
+ }
+ 
+ public String getFirst()
+ {
+     return first; 
+ }
+ 
+ public String getMiddle()
+ {
+     return middle;
+ }
+ 
+ public String getLast()
+ {
+     return last; 
+ }
+ 
+ public LocalTime getActive()
+ {
+     return active;
+ }
+ 
+ public LocalTime getInactive()
+ {
+     return inactive;
+ }
+ 
  
  @Override
  public String toString()
  {
      StringBuilder result =  new StringBuilder(); 
-     result.append("#").append(id).append(" (").append(description).append(")"); 
+     result.append("#").append(id).append(" (").append(last).append(" ").append(first);
+     result.append(" ").append(middle).append(") : ").append(employee).append(", : ");
+     result.append(department).append(", : ").append(shift).append(", : ").append(active); 
+     result.append(inactive).append(","); 
+     
      return result.toString();
  }
 }
